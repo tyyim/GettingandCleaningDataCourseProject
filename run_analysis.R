6ed86e05b7b2007458891ed3843b6bf2320e23f0
Victor Yim <yim.vic@gmail.com>
2016-01-19 19:36:14 +0800

    test

---
+++ run_analysis.R
@@ -0,0 +1,33 @@
+library(dplyr)
+## reading in Training datasets
+TrainX<-read.table("train/X_train.txt",header = FALSE)
+TrainY<-read.table("train/y_train.txt",header = FALSE)
+TrainSub<-read.table("train/subject_train.txt",header = FALSE)
+## reading in Training datasets
+TestX<-read.table("test/X_test.txt",header = FALSE)
+TestY<-read.table("test/y_test.txt",header = FALSE)
+TestSub<-read.table("test/subject_test.txt",header = FALSE)
+
+##Merging Training & Test Datasets
+Feature<-rbind(TrainX,TestX)
+Y<-rbind(TrainY,TestY)
+Subject<-rbind(TrainSub,TestSub)
+
+## reading in index file
+feature_list<-read.table("features.txt",header = FALSE)
+activity_lables<-read.table("activity_labels.txt",header = FALSE)
+
+## using features.txt to lable the variables
+colnames(Feature)<-feature_list[,2]
+
+## de-duplicated column
+Feature<-Feature[!duplicated(colnames(Feature))] %>%
+select(matches("mean|std"))
+
+## merging the activity
+Train_activity<-join(Y,activity_lables,type="inner")
+Full<-cbind(Subject,Train_activity$V2,Feature)
+colnames(Full)[1]<-"Subject"
+colnames(Full)[2]<-"ActivityPerformed"
+By_Act_Sub<-group_by(Full,ActivityPerformed,Subject)
+SummarySet<-summarize_each(By_Act_Sub,funs(mean))
